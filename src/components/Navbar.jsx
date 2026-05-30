12ef40c6365f6e13ddce7396a16b03c8d8f4a76f
maria <mariacnru@gmail.com>
2026-05-30 16:11:32 +0330

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import dayjs from "dayjs";
+import { navLinks, navIcons } from "../constants";
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="">
+        <img src="/public/images/logo.svg" alt="logo" />
+        <p className="font-bold text-sm">Maryam's Portfolio</p>
+
+        <ul>
+          {navLinks.map(({ id, name }) => (
+            <li key={id}>{name}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="">
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img src={img} alt={`icon-${id}`} />
+            </li>
+          ))}
+        </ul>
+
+        <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
